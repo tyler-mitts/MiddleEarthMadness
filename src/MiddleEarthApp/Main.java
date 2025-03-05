8dee5a4b86c16c2b63bd1b7fdf4d540e11590f91
TylerM <TylerM@Tyler>
2025-03-05 00:58:03 -0600

    Tested a character attacking another character

--- src/MiddleEarthApp/Main.java
+++ src/MiddleEarthApp/Main.java
@@ -2,13 +2,18 @@ package MiddleEarthApp;
 
 import character.MiddleEarthCharacter;
 import character.Elf;
+import character.Orc;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Elf George = new Elf("George",100.0,25.0);
+		Orc Bob = new Orc("Bob",150.0,30.0);
 		
-		George.displayInfo();
+		Bob.displayInfo();
+		George.attack(Bob);
+		Bob.displayInfo();
+		
 	}
 
 }
