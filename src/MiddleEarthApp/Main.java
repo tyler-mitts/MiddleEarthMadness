0a32e5d1d5b3e81d454cef4b791462f0da152c1a
TylerM <TylerM@Tyler>
2025-03-05 00:20:39 -0600

    Created Main class and tested displayInfo

---
+++ src/MiddleEarthApp/Main.java
@@ -0,0 +1,14 @@
+package MiddleEarthApp;
+
+import character.MiddleEarthCharacter;
+import character.Elf;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Elf George = new Elf("George",100.0,25.0);
+		
+		George.displayInfo();
+	}
+
+}
