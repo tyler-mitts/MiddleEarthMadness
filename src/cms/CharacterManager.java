beeb876b80dde7979695d9b2a10e3ac7b95bd800
SHenders2424 <s.henderson2424@gmail.com>
2025-03-05 13:15:20 -0600

    Added the rest of the functions to character manager

--- src/cms/CharacterManager.java
+++ src/cms/CharacterManager.java
@@ -23,9 +23,51 @@ public class CharacterManager {
 	public MiddleEarthCharacter getCharacter(String name) {
 		for(int i = 0; i < size; i++) {
 			if(characters[i].getName() == name) {
+				//Character found
 				return characters[i];
 			}
 		}
+		// Character not found
 		return null;
+	}
+	
+	public boolean updateCharacter(MiddleEarthCharacter character, String name, int health, int power) {
+		for(int i = 0; i < size; i++) {
+			if(characters[i] == character) {
+				//Character found
+				characters[i].setName(name);
+				characters[i].setHealth(health);
+				characters[i].setPower(power);
+				return true;
+			}
+			//Character not found
+			return false;
+		}
+	}
+	
+	public boolean deleteCharacter(MiddleEarthCharacter character) {
+		for(int i = 0; i < size; i++) {
+			if(characters[i] == character) {
+				//Character found
+				for(int j = i; j < size - 1; j++) {
+					characters[j] = characters[j + 1];
+				}
+				characters[size-1] = null;
+				size--;
+				return true;
+			}
+		}
+		//Character not found
+		return false;
+	}
+	
+	public void displayAllCharacters() {
+		if(size == 0) {
+			System.out.println("No characters found");
+			return;
+		}
+		for(int i = 0; i < size; i++) {
+			characters[i].displayInfo();
+		}
 	}
 }
