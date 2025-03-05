078f3418040ce1c571f2a32e28a562602c6ba6aa
SHenders2424 <s.henderson2424@gmail.com>
2025-03-05 12:12:21 -0600

    Added first couple methods of CharacterManager
    addCharacter still needs array doubling logic, I will add later

--- src/cms/CharacterManager.java
+++ src/cms/CharacterManager.java
@@ -1,6 +1,31 @@
 package cms;
+import java.util.Arrays;
+
+import character.MiddleEarthCharacter;
 
 public class CharacterManager {
-
+	private MiddleEarthCharacter[] characters;
+	private int size;
 	
+	public CharacterManager(int capacity) {
+		characters = new MiddleEarthCharacter[capacity];
+		size = 0;
+	}
+	
+	public boolean addCharacter(MiddleEarthCharacter c) {
+		if(size == characters.length) {
+			//Logic for doubling array size
+		}
+		characters[size++] = c;
+		return true;
+	}
+	
+	public MiddleEarthCharacter getCharacter(String name) {
+		for(int i = 0; i < size; i++) {
+			if(characters[i].getName() == name) {
+				return characters[i];
+			}
+		}
+		return null;
+	}
 }
